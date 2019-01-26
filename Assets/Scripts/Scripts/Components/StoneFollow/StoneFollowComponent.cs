c3be211124711a637af63adb7160c451f4289e71
Benedikt Kirchmeier <33699887+BeneDev@users.noreply.github.com>
2019-01-26 23:21:10 +0100

    Enemy and planet added

---
+++ Assets/Scripts/Scripts/Components/StoneFollow/StoneFollowComponent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoneFollowComponent : EntityComponent {
+
+    Transform trans;
+    [SerializeField] GameObject followObject;
+    [SerializeField] float followSpeed = 1f;
+
+    public override void InitializeComponent()
+    {
+        updateType = UpdateType.FixedUpdate;
+        trans = GetComponent<MappingComponent>().movementTransform;
+    }
+
+    public override void UpdateComponent()
+    {
+        trans.position = Vector3.Lerp(trans.position, followObject.transform.position, followSpeed);
+        //trans.up = followObject.transform.up;
+    }
+}
